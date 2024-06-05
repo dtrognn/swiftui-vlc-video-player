da8bda049813f8dc6e993dd0460ed8c1ca156b9b
dtrognn <dtrognn@gmail.com>
2024-06-05 10:12:54 +0700

    init project

---
+++ VLC Example/VLC Example/VLC_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VLC_ExampleApp.swift
+//  VLC Example
+//
+//  Created by dtrognn on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct VLC_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
